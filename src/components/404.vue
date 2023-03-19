909a64f5aa7b5aaeb1915a6561af40a3f103db85
aha_yue <895939059@qq.com>
2023-03-19 11:54:04 +0800

    dd

--- src/components/404.vue
+++ src/components/404.vue
@@ -1,11 +1,12 @@
 <template>
-    <div class="error dark:text-white85 text-black85 text-4xl" >404 哦?网络她睡觉了!</div>
+    <div class="error col-span-5 dark:text-white85 text-black85 text-4xl" >404 哦?网络她睡觉了!</div>
 </template>
 
 <style lang="scss" scoped>
 .error {
     width: 100%;
     display: flex;
+    height: 30vh;
     justify-content: center;
     align-items: center;
 
